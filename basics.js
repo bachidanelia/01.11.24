abf8a3089b3be658cc34772bba0eae21f6f6b769
user <bachidanelia@gmail.com>
2024-11-05 18:30:02 +0400

    first assignment

---
+++ basics.js
@@ -0,0 +1,14 @@
+//adding number
+console.log(2+2)
+console.log(3+3)
+
+//multiplying numbers
+console.log(4*4)
+console.log(5*5)
+console.log(6*6)
+
+//writing name
+console.log('Hello Bachi')
+
+//Concatenateing 2 strings
+console.log('this is a first part,' + 'and this is a second part')
